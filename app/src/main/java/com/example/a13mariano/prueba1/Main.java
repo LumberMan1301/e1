e0d380832e060d735f8617bbaa12053de30f282b
Mariano <marianomm@rocketmail.com>
2018-05-08 01:02:52 -0600

    Final

--- app/src/main/java/com/example/a13mariano/prueba1/Main.java
+++ app/src/main/java/com/example/a13mariano/prueba1/Main.java
@@ -7,8 +7,11 @@ import static com.example.a13mariano.prueba1.Sort.BubbleSort.sort;
 public class Main {
     public static void main(String [] args){
         ListaSimple l = new ListaSimple();
+
+        // cantidad de numeros
         int cantidad = (int) (Math.random()*20)+20;
         for(int i = 0; i < cantidad; i++){
+            //numero aleatorio
             int num = (int) (Math.random()*99);
             l.insertar(num);
         }
